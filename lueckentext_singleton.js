8714f384268edc9e935e34c0daa8ad034470ccf6
altay16 <arif.adiguezel@gmail.com>
2023-09-13 08:39:55 +0200

    singleton quiz

---
+++ lueckentext_singleton.js
@@ -0,0 +1,53 @@
+if (document.images) {
+    bild0 = new Image(); bild0.src = "exclamation-circle-solid.svg";
+    bild1 = new Image(); bild1.src = "check-square-solid.svg";
+}
+
+anzluecken = 10;
+
+// Define the correctAnswers array here
+const correctAnswers = [
+    "Logger",
+    "Logger",
+    "static",
+    "getInstance",
+    "null",
+    "Logger",
+    "instance",
+    "Logger",
+    "getInstance()",
+    "log"
+];
+
+function checkAnswers() {
+    const userAnswers = [];
+    let correctAnswersCount = 0;
+
+    for (let i = 1; i <= anzluecken; i++) {
+        const inputName = `text${i}`;
+        const input = document.formular[inputName];
+
+        if (input) {
+            const userAnswer = input.value.trim();
+            userAnswers.push(userAnswer);
+
+            if (userAnswer === "") {
+                document[`auswertung${i}`].src = "exclamation-circle-solid.svg"; // Empty input
+            } else {
+                const correctAnswer = correctAnswers[i - 1];
+                if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+                    document[`auswertung${i}`].src = "check-square-solid.svg"; // Correct answer
+                    correctAnswersCount++;
+                } else {
+                    document[`auswertung${i}`].src = "exclamation-circle-solid.svg"; // Incorrect answer
+                }
+            }
+        }
+    }
+
+    if (correctAnswersCount === anzluecken) {
+        alert(`Glueckwunsch! Alle ${correctAnswersCount}/${anzluecken} Antworten sind richtig! Du Design Pattern Master.`);
+    } else {
+        alert(`${correctAnswersCount}/${anzluecken} Antworten sind richtig. Die Falschen Antworten sind markiert mit einem (!) markiert`);
+    }
+}
